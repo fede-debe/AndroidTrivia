8fdc12017137144377db60b7299c3f61f48f90c1
Federico De Benedictis <66374898+MenneaDb@users.noreply.github.com>
2021-03-14 18:10:21 +0100

    Accomplished to build a graph, connected to the UI, and made navigation happen.

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -18,8 +19,14 @@ class TitleFragment : Fragment() {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_title, container, false)
 
+        // create onClickListener to trigger the action(navigate between the destination) - (in Kotlin we can pass an anonymous function into setOnClickListener)
+        binding.playButton.setOnClickListener (
+            // we call this function on the view to get an instance of the navigation controller
+            /** Navigation.findNavController(it).navigate(R.id.action_titleFragment_to_gameFragment)
+                view.view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)*/
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
         // we need to return binding.root (which contains the root of the layout we’ve just inflated)
         return binding.root
     }
-
 }
